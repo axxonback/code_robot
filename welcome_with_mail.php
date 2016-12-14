5ad1ddb266d3af2da634bb51f6ddc9840172178c
Mars Cao <caojingming@gmail.com>
2016-12-14 13:33:38 +0800

    add email td

---
+++ welcome_with_mail.php
@@ -0,0 +1,46 @@
+<div id="welcomebox">
+<div id="registerbox">
+<h2>Register!</h2>
+<b>Want to coding or analysis? Create an account!</b>
+<form method="POST" action="register.php">
+<table>
+<tr>
+  <td>Username</td><td><input type="text" name="username"></td>
+</tr>
+<tr>
+  <td>Password</td><td><input type="password" name="password"></td>
+</tr>
+<tr>
+  <td>Password (again)</td><td><input type="password" name="password2"></td>
+</tr>
+<tr>
+  <td>E-mail</td><td><input type="text" name="email"></td>
+</tr>
+<tr>
+<td colspan="2" align="right"><input type="submit" name="doit" value="Create an account"></td></tr>
+</table>
+</form>
+<h2>Already registered? Login here</h2>
+<form method="POST" action="login.php">
+<table><tr>
+  <td>Username</td><td><input type="text" name="username"></td>
+  </tr><tr>
+  <td>Password</td><td><input type="password" name="password"></td>
+  </tr><tr>
+  <td colspan="2" align="right"><input type="submit" name="doit" value="Login"></td>
+</tr></table>
+</form>
+</div>
+Hello! Axxonpy is a very simple platform of programming, as a support for programmers on coding. Key points:
+<ul>
+<li>Every login user can submit your request of webstie coding on Axxonpy. You will soon recieve code and scripts of website, and we will help users on their own coding.</li>
+<br>
+<li>The programming work will be seperated into several files. Axxonpy will send them to your registerd E-mail. </li>
+<br>
+<li><strong>So register, login and post your request for website.</strong></li>
+<br>
+<li>We are happy to provide the service of build website and data analysis.</li>
+<br>
+<li>Pls send the email to us, <a href="mailto:data@axxonpy.cn">click here: data@axxonpy.cn</a></li>
+</ul>
+</div>
